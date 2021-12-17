728fc40c0600b3469a3667d70c52cef36394e19b
Axel Raboit <axel.raboit@gmail.com>
2021-12-17 12:56:36 +0100

    make sure when we select a manager, only collaborator of this society appears

--- src/Form/SocietyType.php
+++ src/Form/SocietyType.php
@@ -2,9 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use App\Entity\Society;
+use App\Repository\UserRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -13,6 +16,8 @@ class SocietyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $this->societyId = $options['data']->getId();
+
         $builder
             ->add('name', TextType::class, [
                 'label' => 'Nom'
@@ -28,6 +33,20 @@ class SocietyType extends AbstractType
             ])
             ->add('siret', IntegerType::class, [
                 'label' => 'Siret'
+            ])
+            ->add('manager', EntityType::class, [
+                'label' => 'Gérant',
+                'class' => User::class,
+                'placeholder' => 'Choisir un gérant',
+                'required' => false,
+                'choice_label' => 'email',
+                'query_builder' => function (UserRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->join('u.society','s')
+                        ->where('u.society = :societyId')
+                        ->setParameter('societyId', $this->societyId)
+                        ->orderBy('u.email','ASC');
+                },
             ])
         ;
     }
