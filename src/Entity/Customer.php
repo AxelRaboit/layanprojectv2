ebc310a9add07b0dd38e0a13c0c8635d794bb33e
Axel Raboit <axel.raboit@gmail.com>
2021-12-17 11:09:33 +0100

    feature: new entity customer

---
+++ src/Entity/Customer.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ */
+class Customer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $hasAnswered;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $hasConsumed;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getHasAnswered(): ?bool
+    {
+        return $this->hasAnswered;
+    }
+
+    public function setHasAnswered(bool $hasAnswered): self
+    {
+        $this->hasAnswered = $hasAnswered;
+
+        return $this;
+    }
+
+    public function getHasConsumed(): ?bool
+    {
+        return $this->hasConsumed;
+    }
+
+    public function setHasConsumed(bool $hasConsumed): self
+    {
+        $this->hasConsumed = $hasConsumed;
+
+        return $this;
+    }
+}
